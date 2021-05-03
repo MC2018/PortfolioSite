33a999d28a3b673c192863a520cba3d52e3cf612
= <=>
2021-05-02 20:47:57 -0400

    Started work on a new redesign

--- public/scripts/utils.js
+++ public/scripts/utils.js
@@ -5,19 +5,30 @@ function initialize() {
 
 function loadHeaderFooter() {
     $("body").prepend("<header></header>").append("<footer></footer>");
-    $("header").load("header.html");
-    $("footer").load("footer.html");
+    //$("header").load("header.html");
+    $("footer").load("footer.html", function() {
+        loadSVGs("footer");
+    });
 }
 
-function loadSVGs() {
+function loadSVGs(parent) {
     const svgs = {
         github: "images/github.svg",
         linkedin: "images/linkedin.svg",
-        download: "images/download.svg"
+        download: "images/download.svg",
+        aboutme: "images/aboutme.svg",
+        projects: "images/projects.svg",
+        contact: "images/contact.svg"
     };
 
+    if (parent) {
+        parent += " ";
+    } else {
+        parent = "";
+    }
+
     for (var key in svgs) {
-        $(`.${key}`).load(svgs[key]);
+        $(`${parent}.${key}`).load(svgs[key]);
     }
 }
 
